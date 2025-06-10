21312dc64cb2b7bdba9049cc4329ae14e91becd9
Larry Lei Hua <larry.lei.hua@gmail.com>
2025-06-10 00:13:04 -0500

    rm unif test

--- R/core.R
+++ R/core.R
@@ -1,7 +1,6 @@
 #' Fast inference for bivariate tail dependence and tail asymmetry
 #'
 #' Fast inference for bivariate tail dependence and tail asymmetry. The first run is a bit slower as it needs to load the model, and after the first run the speed will be ultra fast.
-#' @importFrom stats ks.test
 #' @param dat input of uniform scores, should be n rows and 2 columns; if not uniform scores the data will be converted to uniform scores for further calculation.
 #' @param model GGEE or PPPP copulas, default is GGEE copula
 #' @param random Methods for generating random samples used for training NBE, default is quasi
@@ -19,8 +18,7 @@ fasttail <- function(dat, model="GGEE", random="quasi")
   if (ncol(dat) != 2) stop("The data should be n rows and 2 columns!")
   n <- nrow(dat)
 
-  if(ks.test(dat[,1], "punif", min=0, max=1)$p.value < 0.05 |
-     max(dat[,1]) > 1 | min(dat[,1]) < 0)
+  if(max(dat[,1]) > 1 | min(dat[,1]) < 0)
   {
     uscore <- (1:n - 0.5)/n
     cat("The 1st column is not uniformly distributed, and is now
@@ -28,8 +26,7 @@ fasttail <- function(dat, model="GGEE", random="quasi")
     dat[,1] <- uscore[rank(dat[,1])]
   }
 
-  if(ks.test(dat[,2], "punif", min=0, max=1)$p.value < 0.05 |
-     max(dat[,2]) > 1 | min(dat[,2]) < 0)
+  if(max(dat[,2]) > 1 | min(dat[,2]) < 0)
   {
     uscore <- (1:n - 0.5)/n
     cat("The 2nd column is not uniformly distributed, and is now
